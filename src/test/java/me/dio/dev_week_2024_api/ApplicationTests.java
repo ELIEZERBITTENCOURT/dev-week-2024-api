b4b4339b2860330021e659defbb4ab4aa140538b
Eliezer Bittencourt <likobittencourt@gmail.com>
2024-10-21 17:34:07 -0300

    test: teste da aplicação

---
+++ src/test/java/me/dio/dev_week_2024_api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.dev_week_2024_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
